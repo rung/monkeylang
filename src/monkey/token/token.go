8c58d4fef21799294869870df9a487397714cadd
Suezawa <suezawa@gmail.com>
2018-08-25 00:32:45 +0900

    1st commit

---
+++ src/monkey/token/token.go
@@ -0,0 +1,33 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT = "INT" //1,2,3,4
+
+	ASSIGN = "+"
+	PLUS = "+"
+
+	COMMA = ","
+	SEMICORON = ","
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// keyword
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+)
+
+
