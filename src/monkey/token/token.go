06e9e66e1ec3faa4627005f8364298df3ef768a8
rung <suezawa@gmail.com>
2018-08-26 09:03:21 +0900

    modify typo(SEMICORON->SEMICOLON)

--- src/monkey/token/token.go
+++ src/monkey/token/token.go
@@ -3,22 +3,22 @@ package token
 type TokenType string
 
 type Token struct {
-	Type TokenType
+	Type    TokenType
 	Literal string
 }
 
 const (
 	ILLEGAL = "ILLEGAL"
-	EOF = "EOF"
+	EOF     = "EOF"
 
 	IDENT = "IDENT" // add, foobar, x, y, ...
-	INT = "INT" //1,2,3,4
+	INT   = "INT"   //1,2,3,4
 
 	ASSIGN = "="
-	PLUS = "+"
+	PLUS   = "+"
 
-	COMMA = ","
-	SEMICORON = ","
+	COMMA     = ","
+	SEMICOLON = ","
 
 	LPAREN = "("
 	RPAREN = ")"
@@ -27,8 +27,5 @@ const (
 
 	// keyword
 	FUNCTION = "FUNCTION"
-	LET = "LET"
+	LET      = "LET"
 )
-
-
-
