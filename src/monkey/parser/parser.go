2985f402b6b522efe2e19fe03f3de267fd521dad
rung <suezawa@gmail.com>
2018-09-03 14:24:11 +0900

    add parser of IfExpression

--- src/monkey/parser/parser.go
+++ src/monkey/parser/parser.go
@@ -39,6 +39,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.TRUE, p.parseBoolean)
 	p.registerPrefix(token.FALSE, p.parseBoolean)
 	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
+	p.registerPrefix(token.IF, p.parseIfExpression)
 
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
@@ -315,4 +316,57 @@ func (p *Parser) parseGroupedExpression() ast.Expression {
 	}
 
 	return exp
+}
+
+// ifexpression
+// if ( x ){ a } else {b}
+func (p *Parser) parseIfExpression() ast.Expression {
+	// if
+	expression := &ast.IfExpression{Token: p.curToken}
+	// (
+	if !p.expectPeek(token.LPAREN) {
+		return nil
+	}
+	p.nextToken()
+	// 普通にexpressionを処理する(expressionStatementと同様)
+	// x
+	expression.Condition = p.parseExpression(LOWEST)
+
+	// )
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	if !p.expectPeek(token.LBRACE) {
+		return nil
+	}
+
+	expression.Consequence = p.parseBlockStatement()
+
+	if !p.expectPeek(token.RBRACE) {
+		return nil
+	}
+
+	// TODO: else未実装
+
+	return expression
+
+}
+
+func (p *Parser) parseBlockStatement() *ast.BlockStatement {
+	block := &ast.BlockStatement{Token: p.curToken}
+	block.Statements = []ast.Statement{}
+
+	p.nextToken()
+
+	// this is like ParseProgram()
+	for !p.curTokenIs(token.RBRACE) && !p.curTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			block.Statements = append(block.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return block
 }
