9a85e77d4da69b80fa3d45b052b8a30fadeb9679
rung <suezawa@gmail.com>
2018-09-07 15:49:05 +0900

    add minusprefix evaluator

--- src/monkey/evaluator/evaluator.go
+++ src/monkey/evaluator/evaluator.go
@@ -61,6 +61,9 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 	case "!":
 		return evalBangOperatorExpression(right)
 
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
+
 	default:
 		return NULL
 	}
@@ -77,4 +80,13 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 	default:
 		return FALSE
 	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
 }
