8d927e3dadbbc2091a25440383c3a1abcf519a80
rung <suezawa@gmail.com>
2018-09-07 15:41:36 +0900

    add boolean and prefix evaluator

--- src/monkey/evaluator/evaluator.go
+++ src/monkey/evaluator/evaluator.go
@@ -5,8 +5,21 @@ import (
 	"monkey/object"
 )
 
+// メモリ節約のためにtrue, falseへのポインタを共有するという説明があるP130
+// しかしながらこれでのメモリ節約は微々たるもの。どちらかというと、
+// あとで比較などをするときに一緒のポインタ見てると間違えなくて楽というメリットはある？
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+	NULL  = &object.Null{}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
+
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 
 	case *ast.Program:
 		return evalStatements(node.Statements)
@@ -16,6 +29,9 @@ func Eval(node ast.Node) object.Object {
 
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+
+	case *ast.Boolean:
+		return nativeBooleanObject(node.Value)
 
 	}
 
@@ -31,4 +47,34 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
 }
