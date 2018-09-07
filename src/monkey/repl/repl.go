9a7d27a0f6d0702c5423f284f8ce26ef91946689
rung <suezawa@gmail.com>
2018-09-07 15:59:05 +0900

    REPL shows me AST.

--- src/monkey/repl/repl.go
+++ src/monkey/repl/repl.go
@@ -30,6 +30,8 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
 
 		evaluated := evaluator.Eval(program)
 		if evaluated != nil {
