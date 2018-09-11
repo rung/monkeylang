20531e420ee8ffd90ba05f8ce699c6fe7fec9d3e
rung <suezawa@gmail.com>
2018-09-11 14:39:56 +0900

    add FunctionLiteral evaluator

--- src/monkey/evaluator/evaluator.go
+++ src/monkey/evaluator/evaluator.go
@@ -70,6 +70,11 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 
 	case *ast.Identifier:
 		return evalIdentifier(node, env)
+
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{Parameters: params, Env: env, Body: body}
 	}
 
 	return nil
