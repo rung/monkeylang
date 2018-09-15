1b41aa6c79e0d18b3af29b6ae43648cce70a09ae
rung <suezawa@gmail.com>
2018-09-15 16:00:27 +0900

    extend builtin function(len).  supporting len(array)

--- src/monkey/evaluator/evaluator_test.go
+++ src/monkey/evaluator/evaluator_test.go
@@ -361,6 +361,8 @@ func TestBuiltinFunctions(t *testing.T) {
 		{`len("hello world")`, 11},
 		{`len(1)`, "argument to `len` not supported, got INTEGER"},
 		{`len("one", "two")`, "wrong number of arguments. got=2, want=1"},
+		{`len([1, 2, 3])`, 3},
+		{`let a = [1, true, "3"]; len(a)`, 3},
 	}
 
 	for _, tt := range tests {
