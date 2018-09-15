1c986e3bcfb31b52907a140ca5616bf33785d9f3
rung <suezawa@gmail.com>
2018-09-16 06:58:38 +0900

    ファイル読み込み対応

--- src/monkey/main.go
+++ src/monkey/main.go
@@ -2,18 +2,59 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
+	"monkey/evaluator"
+	"monkey/lexer"
+	"monkey/object"
+	"monkey/parser"
 	"monkey/repl"
 	"os"
 	"os/user"
 )
 
 func main() {
-	user, err := user.Current()
+
+	if os.Args[1] == "repl" {
+		user, err := user.Current()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Hello %s\n", user.Username)
+		fmt.Printf("Feel free to type in commands\n")
+		repl.Start(os.Stdin, os.Stdout)
+		return
+	}
+
+	fp, err := os.Open(os.Args[1])
 	if err != nil {
 		panic(err)
 	}
+	line, err := ioutil.ReadAll(fp)
+	if err != nil {
+		panic(err)
+	}
+	env := object.NewEnvironment()
 
-	fmt.Printf("Hello %s\n", user.Username)
-	fmt.Printf("Feel free to type in commands\n")
-	repl.Start(os.Stdin, os.Stdout)
+	l := lexer.New(string(line))
+	p := parser.New(l)
+
+	program := p.ParseProgram()
+	if len(p.Errors()) != 0 {
+		printParserErrors(os.Stdout, p.Errors())
+	}
+
+	evaluated := evaluator.Eval(program, env)
+	if evaluated != nil {
+		fmt.Println(evaluated.Inspect())
+	}
+
+	return
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
 }
