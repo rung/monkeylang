33d3c10b51be100cb233f1d533529b07c81ff7f0
rung <suezawa@gmail.com>
2018-09-18 21:48:59 +0900

    add hash object

--- src/monkey/object/object.go
+++ src/monkey/object/object.go
@@ -176,10 +176,29 @@ type Hash struct {
 }
 
 func (h *Hash) Type() ObjectType { return HASH_OBJ }
+func (h *Hash) Inspect() string {
+	var out bytes.Buffer
+
+	pairs := []string{}
+	for _, pair := range h.Pairs {
+		pairs = append(pairs, fmt.Sprintf("%s: %s",
+			pair.Key.Inspect(), pair.Value.Inspect()))
+	}
+
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", l"))
+	out.WriteString("}")
+
+	return out.String()
+}
 
 type HashKey struct {
 	Type  ObjectType
 	Value uint64
+}
+
+type Hashable interface {
+	HashKey() HashKey
 }
 
 func (b *Boolean) HashKey() HashKey {
--- src/monkey/object/object_test.go
+++ src/monkey/object/object_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestStringHashKey(t *testing.T) {
 	hello1 := &String{Value: "Hello world"}
 	hello2 := &String{Value: "Hello world"}
-	diff1 := &String{Value: "my name is johnny"}
+	diff1 := &String{Value: "My name is johnny"}
 	diff2 := &String{Value: "My name is johnny"}
 
 	if hello1.HashKey() != hello2.HashKey() {
