f70e74ad24615ae57a447ea752279e7707960e54
rung <suezawa@gmail.com>
2018-09-20 09:59:37 +0900

    add hash evaluator

--- src/monkey/evaluator/evaluator.go
+++ src/monkey/evaluator/evaluator.go
@@ -105,7 +105,7 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 	case *ast.HashLiteral:
 		return evalHashLiteral(node, env)
 
-	case *ast.IndexExpression: //Array呼び出し
+	case *ast.IndexExpression: //Array, Hash呼び出し
 		left := Eval(node.Left, env)
 		if isError(left) {
 			return left
@@ -361,11 +361,13 @@ func unwrapReturnValue(obj object.Object) object.Object {
 	return obj
 }
 
-// Array index
+// Array, Hash index
 func evalIndexExpression(left, index object.Object) object.Object {
 	switch {
 	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
 		return evalArrayIndexExpression(left, index)
+	case left.Type() == object.HASH_OBJ:
+		return evalHashIndexExpression(left, index)
 	default:
 		return newError("index operator not supported: %s", left.Type())
 	}
@@ -409,6 +411,21 @@ func evalHashLiteral(node *ast.HashLiteral, env *object.Environment) object.Obje
 	}
 
 	return &object.Hash{Pairs: pairs}
+}
+
+func evalHashIndexExpression(hash, index object.Object) object.Object {
+	hashObject := hash.(*object.Hash)
+
+	key, ok := index.(object.Hashable)
+	if !ok {
+		return newError("unusable as hash key: %s", index.Type())
+	}
+
+	pair, ok := hashObject.Pairs[key.HashKey()]
+	if !ok {
+		return NULL
+	}
+	return pair.Value
 }
 
 // error
