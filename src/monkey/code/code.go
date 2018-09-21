b40b25538ff8c7a2194ac09fd8906ddc744bd7de
rung <suezawa@gmail.com>
2018-09-21 14:40:21 +0900

    add "Make" to code

--- src/monkey/code/code.go
+++ src/monkey/code/code.go
@@ -1,6 +1,9 @@
 package code
 
-import "fmt"
+import (
+	"encoding/binary"
+	"fmt"
+)
 
 type Instructions []byte
 
@@ -26,4 +29,34 @@ func Lookup(op byte) (*Definition, error) {
 	}
 
 	return def, nil
+}
+
+func Make(op Opcode, operands ...int) []byte {
+	def, ok := definitions[op]
+	if !ok {
+		return []byte{}
+	}
+
+	instructionLen := 1
+	for _, w := range def.OperandWidths {
+		instructionLen += w
+	}
+
+	// instruction {Opcode(1byte), argument1(1byte), argument2(1byte)}
+	instruction := make([]byte, instructionLen)
+	instruction[0] = byte(op)
+
+	offset := 1
+	// instructionに順番に値を突っ込んでいく(big endianで値は渡される)
+	for i, o := range operands {
+		width := def.OperandWidths[i]
+		switch width {
+		case 2:
+			// PutUint16 put uint16 to []byte
+			binary.BigEndian.PutUint16(instruction[offset:], uint16(o))
+		}
+		offset += width
+	}
+
+	return instruction
 }
