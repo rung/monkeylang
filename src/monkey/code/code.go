19550c1434821c8e88575ee061f0736eeda4ce11
rung <suezawa@gmail.com>
2018-09-21 09:57:31 +0900

    start reading "Writing A Compiler In Go"!

---
+++ src/monkey/code/code.go
@@ -0,0 +1,29 @@
+package code
+
+import "fmt"
+
+type Instructions []byte
+
+type Opcode byte
+
+const (
+	OpConstant Opcode = iota
+)
+
+type Definition struct {
+	Name          string
+	OperandWidths []int
+}
+
+var definitions = map[Opcode]*Definition{
+	OpConstant: {"OpConstant", []int{2}},
+}
+
+func Lookup(op byte) (*Definition, error) {
+	def, ok := definitions[Opcode(op)]
+	if !ok {
+		return nil, fmt.Errorf("opcode %d undefined", op)
+	}
+
+	return def, nil
+}
