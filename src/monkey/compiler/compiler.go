6e42ac6423e76245bb229ce9b3876d9b7b98bf68
rung <suezawa@gmail.com>
2018-09-23 01:42:14 +0900

    add minimal compiler

---
+++ src/monkey/compiler/compiler.go
@@ -0,0 +1,38 @@
+package compiler
+
+import (
+	"monkey/ast"
+	"monkey/code"
+	"monkey/object"
+)
+
+type Compiler struct {
+	instructions code.Instructions
+	constants    []object.Object
+}
+
+// New makes new Compiler struct
+func New() *Compiler {
+	return &Compiler{
+		instructions: code.Instructions{},
+		constants:    []object.Object{},
+	}
+}
+
+// Memo: instructionsとconstantsを埋めていく？
+//  定数の保存と、バイトコードの生成
+func (c *Compiler) Compile(node ast.Node) error {
+	return nil
+}
+
+func (c *Compiler) Bytecode() *Bytecode {
+	return &Bytecode{
+		Instructions: c.instructions,
+		Constants:    c.constants,
+	}
+}
+
+type Bytecode struct {
+	Instructions code.Instructions
+	Constants    []object.Object
+}
