7c92ad13fac5a5e8f0099cbaac583a043331d34c
rung <suezawa@gmail.com>
2018-09-24 12:35:52 +0900

    add feature of compiling OpConstant

--- src/monkey/compiler/compiler.go
+++ src/monkey/compiler/compiler.go
@@ -21,7 +21,37 @@ func New() *Compiler {
 
 // Memo: instructionsとconstantsを埋めていく？
 //  定数の保存と、バイトコードの生成
+//  evaluatorと似た書き方でastを探索していく
 func (c *Compiler) Compile(node ast.Node) error {
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, s := range node.Statements {
+			err := c.Compile(s)
+			if err != nil {
+				return err
+			}
+		}
+	case *ast.ExpressionStatement:
+		err := c.Compile(node.Expression)
+		if err != nil {
+			return err
+		}
+	case *ast.InfixExpression:
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+		}
+
+		err = c.Compile(node.Right)
+		if err != nil {
+			return err
+		}
+
+	case *ast.IntegerLiteral:
+		integer := &object.Integer{Value: node.Value}
+		// emitにOpConstantと、OpConstantのindex(addConstatntの戻り値)を渡す
+		c.emit(code.OpConstant, c.addConstant(integer))
+	}
 	return nil
 }
 
@@ -35,4 +65,25 @@ func (c *Compiler) Bytecode() *Bytecode {
 type Bytecode struct {
 	Instructions code.Instructions
 	Constants    []object.Object
+}
+
+// addConstant add Object to "constants", and return index in the "constants"
+func (c *Compiler) addConstant(obj object.Object) int {
+	c.constants = append(c.constants, obj)
+	return len(c.constants) - 1
+}
+
+// emit writs bytecode, and return the starting position of the just-emitted instruction.
+// Memo: バイトコードをcompiler構造体の中のinstructionsに書き込む.その後、書き込んだバイトコードの開始indexを返す.
+func (c *Compiler) emit(op code.Opcode, operands ...int) int {
+	ins := code.Make(op, operands...)
+	pos := c.addInstruction(ins)
+	return pos
+}
+
+func (c *Compiler) addInstruction(ins []byte) int {
+	// Memo:  返す値は書き込む前のlen
+	posNewInstruction := len(c.instructions)
+	c.instructions = append(c.instructions, ins...)
+	return posNewInstruction
 }
