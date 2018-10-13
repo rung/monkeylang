1f01cf99fff48809bca99cbae9952372c5a35c31
rung <suezawa@gmail.com>
2018-10-14 06:13:20 +0900

    add Frame stack(namespace)  and some bytecodes(OpCall, etc)  (vm)

--- src/monkey/vm/vm.go
+++ src/monkey/vm/vm.go
@@ -207,6 +207,26 @@ func (vm *VM) Run() error {
 			if err != nil {
 				return err
 			}
+
+		case code.OpCall:
+			fn, ok := vm.stack[vm.sp-1].(*object.CompiledFunction)
+			if !ok {
+				return fmt.Errorf("calling non-function")
+			}
+			frame := NewFrame(fn)
+			vm.pushFrame(frame)
+
+		case code.OpReturnValue:
+			returnValue := vm.pop()
+
+			vm.popFrame()
+			// pop (*object.CompiledFunction)
+			vm.pop()
+
+			err := vm.push(returnValue)
+			if err != nil {
+				return err
+			}
 		}
 	}
 	return nil
