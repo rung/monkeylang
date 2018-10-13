033f7b24e9d65ba4e2f164a24de32b4a93dfc0d0
rung <suezawa@gmail.com>
2018-10-14 06:27:14 +0900

    add test(for calling function) (vm_test)

--- src/monkey/vm/vm_test.go
+++ src/monkey/vm/vm_test.go
@@ -311,6 +311,37 @@ func TestCallingFunctionsWithoutArguments(t *testing.T) {
 		`,
 			expected: 15,
 		},
+		{
+			input: `
+				let one = fn() { 1; };
+				let two = fn() { 2; };
+				one() + two()
+				`,
+			expected: 3,
+		},
+		{
+			input: `
+			let a = fn() { 1 };
+			let b = fn() { a() + 1 };
+			let c = fn() { b() + 1 };
+			c();
+			`,
+			expected: 3,
+		},
+		{
+			input: `
+			let earlyExit = fn() { return 99; 100; };
+			earlyExit();
+			`,
+			expected: 99,
+		},
+		{
+			input: `
+			let earlyExit = fn() { return 99; return 100; };
+			earlyExit();
+			`,
+			expected: 99,
+		},
 	}
 
 	runVmTests(t, tests)
