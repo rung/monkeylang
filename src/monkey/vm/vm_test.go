5dea9c8422b03a3422b493ff3daa414141eb0089
rung <suezawa@gmail.com>
2018-10-14 16:21:02 +0900

    add function test (vm)

--- src/monkey/vm/vm_test.go
+++ src/monkey/vm/vm_test.go
@@ -369,3 +369,17 @@ func TestFunctionsWithoutReturnValue(t *testing.T) {
 
 	runVmTests(t, tests)
 }
+
+func TestFirstClassFunctions(t *testing.T) {
+	tests := []vmTestCase{
+		{
+			input: `
+			let returnsOne = fn() { 1; };
+			let returnsOneReturner = fn() { returnsOne; };
+			returnsOneReturner()();
+			`,
+			expected: 1,
+		},
+	}
+	runVmTests(t, tests)
+}
